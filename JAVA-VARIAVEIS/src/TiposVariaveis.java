0c549ffd17c94b8995867c7fb58e74b9af508295
ThiagoGs-Dev <96384024+ThiagoGs-Dev@users.noreply.github.com>
2024-02-17 18:22:59 -0300

    Tipos de Variáveis (finished)

--- JAVA-VARIAVEIS/src/TiposVariaveis.java
+++ JAVA-VARIAVEIS/src/TiposVariaveis.java
@@ -1,3 +1,21 @@
+// Tipos primitivos 8 (oito).
+// Não são Objetos.
 public class TiposVariaveis {
-    
+    // Class String que representa textos
+    String Nome = "Thiago";
+
+    int idade = 25;
+    // Note o sufixo 'L' para indicar que é um longo.
+    long populacaoMundial =  idade;  
+    // Note o sufixo 'f' para indicar que é um float.
+    float altura = 1.75f;  
+    double pi = 3.141592653589793;
+    char letra = 'A';
+    boolean estaChovendo = false;
+    // Constantes em Java
+    final double VALOR_DE_PI = 3.14;
+
+    public static void main(String[] args) {
+
+    }
 }
