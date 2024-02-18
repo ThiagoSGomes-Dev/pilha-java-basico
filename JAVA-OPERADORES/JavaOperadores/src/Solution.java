56a567c197fb0e50d9a6baf9efd885dbd22f8618
ThiagoGs-Dev <96384024+ThiagoGs-Dev@users.noreply.github.com>
2024-02-17 22:10:29 -0300

    Desafio (HackerRank-01)

---
+++ JAVA-OPERADORES/JavaOperadores/src/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int oneInt = scanner.nextInt();
+    int twoInt = scanner.nextInt();
+    int threeInt = scanner.nextInt();
+    scanner.close();
+
+
+    System.out.println("oneInt is: " + oneInt);
+    System.out.println("twoInt is: " + twoInt);
+    System.out.println("threeInt is: " + threeInt);
+    }
+}
